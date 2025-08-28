f838d57f8d47b3b1a892d927a65d304501f6aac8
peter-sp-1 <samuelpeteropeyemi@gmail.com>
2025-08-28 16:03:45 +0100

    live production

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FellowshipAttendanceApp from "./FellowshipAttendanceApp";
-import AttendanceForm from "./AttendanceForm";
+import { Routes, Route } from 'react-router-dom';
+import FellowshipAttendanceApp from './FellowshipAttendanceApp.jsx';
+import AttendanceForm from './AttendanceForm.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Main dashboard (what you pasted above) */}
-        <Route path="/" element={<FellowshipAttendanceApp />} />
-
-        {/* Page users see when scanning QR code */}
-        <Route path="/attend/:sessionId" element={<AttendanceForm />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<FellowshipAttendanceApp />} />
+      <Route path="/attend/:sessionId" element={<AttendanceForm />} />
+    </Routes>
   );
 }
