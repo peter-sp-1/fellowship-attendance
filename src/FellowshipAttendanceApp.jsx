3176aed8b91592c9346946a4bc0713339d1341da
peter-sp-1 <samuelpeteropeyemi@gmail.com>
2025-08-30 00:48:21 +0100

    fellowshipAttendance.js updated

--- src/FellowshipAttendanceApp.jsx
+++ src/FellowshipAttendanceApp.jsx
@@ -500,7 +500,7 @@ const FellowshipAttendanceApp = () => {
                     )}
                   </div>
                   <button
-                    onClick={() => removeMember(member.id)}
+                    onClick={() => removeMember(member._id)}
                     className="p-2 text-red-600 hover:bg-red-50 rounded-lg transition-colors"
                   >
                     <Trash2 className="w-4 h-4" />
